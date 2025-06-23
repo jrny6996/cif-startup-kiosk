9b70b5e3e41c9a44898fdf8fac4f93708eb47a01
jrny6996 <thejonnyc9@gmail.com>
2025-06-23 17:27:02 -0400

    updates for hector

--- kiosk.py
+++ kiosk.py
@@ -1,24 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 import time
 
-# returns options to be passed into webdriver.Edge()
-def get_default_edge_options():
-    options = webdriver.EdgeOptions()
-    options.add_argument("--start-maximized")
-    return options
+
 
 def run_and_refresh_page(url:str, sleep_time:float=3)->None:
-    options = get_default_edge_options()
+    options=""
+    options = Options()
+    options.add_argument("--start-fullscreen")
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    options.add_experimental_option('useAutomationExtension', False)
     driver = webdriver.Edge(options=options)
 
     driver.get(url)
-
+    driver.implicitly_wait(3)
     while True:
         time.sleep(sleep_time)
         driver.refresh()
 
     driver.quit()
 if __name__ == "__main__":
-    url= "https://google.com"
-    run_and_refresh_page(url)
+    url= "https://rutgers.my.site.com/OneStopWalkIn/s/newarkwalkinstatus"
+    run_and_refresh_page(url, sleep_time=60)
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Scripts for running browsers
+
+Included file in `\dist\ez-run.exe\ez-run`
+
+## Building
+
+1. Build python Virtual Envirnment(optional) `python -m venv env` & activate it `.\env\Scripts\activate`
+2. Install dependancies `pip install -r requirements.txt`
+3. Run `python kiosk.py` to run the appicaltion
+4. Build an executable file using pyinstaller `pyinstaller kiosk.py --name ez-run.exe` which will output the .exe file in `\dist\ez-run.exe\ez-run`
+
+## Kiosks @ Conklin
+
+These should run the kiosk.py script which is the one setup in `.\ez-run.exe` by default
